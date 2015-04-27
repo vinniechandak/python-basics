6ad223a6796137aa27f17ed16f936734f3354274
Vinod Chandak <vinniechandak@gmail.com>
2015-04-27 22:43:00 +0530

    operator's explaination.

---
+++ operators.py
@@ -0,0 +1,35 @@
+a = 5;
+b = 2;
+c = a+b;
+d = a/float(b);
+e = float(a)/b;
+f = a / b;
+g = a * b;
+h = a - b;
+i = a ** b;
+j = ++a;
+k = --b;
+
+l = True;
+m = False;
+
+o = not l;
+p = not 0;
+q = not 1;
+
+print "a : ", a;
+print "b : ", b;
+print "Addition : ", c;
+print "Divison (Float Divider) :", d;
+print "Divison (Float Dividend) :", e;
+print "Divison :", f;
+print "Multiplication : ", g;
+print "Subtraction : ", h;
+print "Power : ", i;
+print "Increment : ", j; # no effect, No Increment Operator in Python !!!
+print "Decrement : ", k; # no effect, No Decrement Operator in Python !!!
+print "l : ", l;
+print "m : ", m;
+print "Negation of l : ", o;
+print "Negation of 0 : ", p;
+print "Negation of 1 : ", q;
