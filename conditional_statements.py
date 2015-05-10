8cae03fb5983aec98755591d36db6dab8acce0e1
Vinod Chandak <vinniechandak@gmail.com>
2015-05-10 17:28:07 +0530

    conditional structure in python.

---
+++ conditional_statements.py
@@ -0,0 +1,26 @@
+# conditional statements in python
+
+a = int(raw_input("Enter value 1:"));
+b = int(raw_input("Enter value 2:"));
+
+print """Enter Choice:
+1. sum
+2. subtract
+3. multiply
+4. division
+5. modulus"""
+
+choice = int(raw_input());
+
+if choice == 1:
+	print "Sum:", a+b;
+elif choice == 2:
+	print "Subtraction:", a-b;
+elif choice == 3:
+	print "Multiplication:", a*b;
+elif choice == 4:
+	print "Division:", a/b;
+elif choice == 5:
+	print "Modulus:", a%b;
+else:
+	print 'Invalid choice !!!'
