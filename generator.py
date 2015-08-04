162f4dee71c73816a6ec72e5cfa20dd579a6f7f8
vinniechandak <vinniechandak@gmail.com>
2015-08-05 00:53:20 +0530

    commiting modified file.

--- generator.py
+++ generator.py
@@ -4,3 +4,13 @@ def simplegen():
 	yield 'ccc'
 a = simplegen();
 print(a);
+for item in a:
+	print item
+
+a = [lambda x:x,
+lambda x: x**2,
+lambda x:x**3]
+
+print(a[0](3));
+print(a[1](3));
+print(a[2](3));
