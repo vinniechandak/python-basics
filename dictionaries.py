12cd897d192974242815524fbe71116256008755
Vinod Chandak <vinniechandak@gmail.com>
2018-07-20 00:46:06 +0530

    WIP: making python 3 compatible
    WIP: making python 3 compatible

--- dictionaries.py
+++ dictionaries.py
@@ -1,16 +1,16 @@
 # This is about dictionary data structure in python.
 d = {'a': 'b', 'c': 'd', 'e': 'f'};
-print(d);
+print("Print Dictionary ==> " , d);
 
 d['Vinod'] = 'Chandak';  # appending element to dictionary
-print(d);
+print("Print Amended Dictionary ==> " , d);
 
 d['a'] = 'Modified key a';  # value overriding.
-print(d);
+print("Print Value Overridden Dictionary ==> ",d);
 
-print(d.items());  # print items of a dictionary in a tuple format.
-print(d.keys());  # print keys of a dictionary.
-print(d.values());  # print values of a dictionary.
+print("Print Dictionary as a Tuple ==> " , d.items());  # print items of a dictionary in a tuple format.
+print("Print Dictionary Keys ==> " ,d.keys());  # print keys of a dictionary.
+print("Print Dictionary Values ==> " ,d.values());  # print values of a dictionary.
 
-print(d.__contains__('a'));  # checking whether key exists in dictionary or not.
-print(d.get('Vinod'));  # get value by key. if key doesn't exists it returns None.
+print("Print Dictionary Contains Key? ==> " ,d.__contains__('a'));  # checking whether key exists in dictionary or not.
+print("Print Dictionary Value by Key ==> " ,d.get('Vinod'));  # get value by key. if key doesn't exists it returns None.
--- files.py
+++ files.py
@@ -2,18 +2,15 @@
 f = open('myfile.txt', 'w');
 f.write('This is a text message inside a file.');  # writing content to file.
 f.close();
-print
-f;
+print("File =>" ,f);
 
 with open('myfile.txt', 'r') as content:  # reading data from existing file.
     for data in content:
-        print
-        data;
+        print(data);
 
 import zipfile;
 
 outfile = open('myzip.zip', 'wb');
 zfile = zipfile.ZipFile(outfile, 'w', zipfile.ZIP_DEFLATED);
 zfile.writestr('data1', 'This is zip file');
-print
-zfile;
+print("Zipped File =>" ,zfile);
