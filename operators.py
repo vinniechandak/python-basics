81a9137d8f7903375788fd4151a430e8823a4d8f
Vinod Chandak <vinniechandak@gmail.com>
2018-07-20 20:56:59 +0530

    making python 3 compatible
    making python 3 compatible

--- operators.py
+++ operators.py
@@ -17,35 +17,19 @@ o = not l;
 p = not 0;
 q = not 1;
 
-print
-"a : ", a;
-print
-"b : ", b;
-print
-"Addition : ", c;
-print
-"Divison (Float Divider) :", d;
-print
-"Divison (Float Dividend) :", e;
-print
-"Divison :", f;
-print
-"Multiplication : ", g;
-print
-"Subtraction : ", h;
-print
-"Power : ", i;
-print
-"Increment : ", j;  # no effect, No Increment Operator in Python !!!
-print
-"Decrement : ", k;  # no effect, No Decrement Operator in Python !!!
-print
-"l : ", l;
-print
-"m : ", m;
-print
-"Negation of l : ", o;
-print
-"Negation of 0 : ", p;
-print
-"Negation of 1 : ", q;
+print("a : ", a);
+print("b : ", b);
+print("Addition : ", c);
+print("Divison (Float Divider) :", d);
+print("Divison (Float Dividend) :", e);
+print("Divison :", f);
+print("Multiplication : ", g);
+print("Subtraction : ", h);
+print("Power : ", i);
+print("Increment : ", j);  # no effect, No Increment Operator in Python !!!
+print("Decrement : ", k);  # no effect, No Decrement Operator in Python !!!
+print("l : ", l);
+print("m : ", m);
+print("Negation of l : ", o);
+print("Negation of 0 : ", p);
+print("Negation of 1 : ", q);
